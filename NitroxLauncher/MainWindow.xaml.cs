90afc8729d2d194008e0dd93b7c8b1c9b3556529
MrVeil303 <50862104+MrVeil303@users.noreply.github.com>
2021-03-27 17:09:56 +0200

    See description 001
    Pirate subnautica support added
    Pirate Nitrox logo added
    Nitrox logo changed

--- NitroxLauncher/MainWindow.xaml.cs
+++ NitroxLauncher/MainWindow.xaml.cs
@@ -6,162 +6,152 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using NitroxLauncher.AttachedProperties;
 using NitroxLauncher.Events;
 using NitroxLauncher.Pages;
+using NitroxLauncher.Patching;
 using NitroxModel.Discovery;
 using NitroxModel.Helper;
 
 namespace NitroxLauncher
 {
-    public partial class MainWindow : Window, INotifyPropertyChanged
+  public partial class MainWindow : Window, INotifyPropertyChanged
+  {
+    private readonly LauncherLogic logic = new LauncherLogic();
+    private object frameContent;
+    private bool isServerEmbedded;
+
+    public string Version => $"{LauncherLogic.RELEASE_PHASE} {LauncherLogic.Version}";
+
+    public object FrameContent
     {
-        private readonly LauncherLogic logic = new LauncherLogic();
-        private object frameContent;
-        private bool isServerEmbedded;
+      get => frameContent;
+      set
+      {
+        frameContent = value;
 
-        public string Version => $"{LauncherLogic.RELEASE_PHASE} {LauncherLogic.Version}";
-
-        public object FrameContent
+        // Update navigation buttons styling
+        foreach (Button button in SideBarPanel.GetChildrenOfType<Button>())
         {
-            get => frameContent;
-            set
-            {
-                frameContent = value;
-
-                // Update navigation buttons styling
-                foreach (Button button in SideBarPanel.GetChildrenOfType<Button>())
-                {
-                    button.SetValue(ButtonProperties.SelectedProperty, button.Tag == value || button.Tag?.GetType() == typeof(ServerPage) && value?.GetType() == typeof(ServerConsolePage));
-                }
-
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(CurrentPageBackground));
-            }
+          button.SetValue(ButtonProperties.SelectedProperty, button.Tag == value || button.Tag?.GetType() == typeof(ServerPage) && value?.GetType() == typeof(ServerConsolePage));
         }
 
-        public ImageSource CurrentPageBackground => (ImageSource)(FrameContent as Page)?.Background.GetValue(ImageBrush.ImageSourceProperty);
-
-        public MainWindow()
-        {
-            InitializeComponent();
-
-            // Pirate trigger should happen after UI is loaded.
-            Loaded += (sender, args) =>
-            {
-                // This pirate detection subscriber is immediately invoked if pirate has been detected right now.
-                PirateDetection.PirateDetected += (o, eventArgs) =>
-                {
-                    WebBrowser webBrowser = new WebBrowser();
-                    FrameContent = webBrowser;
-                    webBrowser.HorizontalAlignment = HorizontalAlignment.Stretch;
-                    webBrowser.VerticalAlignment = VerticalAlignment.Stretch;
-                    webBrowser.Margin = new Thickness(0);
-
-                    string embed = "<html><head>" +
-                                   "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\"/>" +
-                                   "</head><body>" +
-                                   $"<iframe width=\"{MainFrame.ActualWidth}\" height=\"{MainFrame.ActualHeight}\" src=\"{{0}}\"" +
-                                   "frameborder = \"0\" allow = \"autoplay; encrypted-media\" allowfullscreen></iframe>" +
-                                   "</body></html>";
-                    webBrowser.NavigateToString(string.Format(embed, "https://www.youtube.com/embed/i8ju_10NkGY?autoplay=1"));
-                    SideBarPanel.Visibility = Visibility.Hidden;
-                };
-            };
-
-            logic.ServerStarted += ServerStarted;
-            logic.ServerExited += ServerExited;
-
-            logic.SetTargetedSubnauticaPath(GameInstallationFinder.Instance.FindGame())
-                .ContinueWith(task =>
-                    {
-                        if (logic.IsSubnauticaDirectory(task.Result))
-                        {
-                            LauncherLogic.Instance.NavigateTo<LaunchGamePage>();
-                        }
-                        else
-                        {
-                            LauncherLogic.Instance.NavigateTo<OptionPage>();
-                        }
-
-                        logic.CheckNitroxVersion();
-                    },
-                    CancellationToken.None,
-                    TaskContinuationOptions.OnlyOnRanToCompletion,
-                    TaskScheduler.FromCurrentSynchronizationContext());
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private bool CanClose()
-        {
-            if (logic.ServerRunning && isServerEmbedded)
-            {
-                MessageBoxResult userAnswer = MessageBox.Show("The embedded server is still running. Click yes to close.", "Close aborted", MessageBoxButton.YesNo, MessageBoxImage.Warning);
-                return userAnswer == MessageBoxResult.Yes;
-            }
-
-            return true;
-        }
-
-        private void OnClosing(object sender, CancelEventArgs e)
-        {
-            if (!CanClose())
-            {
-                e.Cancel = true;
-                return;
-            }
-            logic.Dispose();
-        }
-
-        private void Close_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void Minimze_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
-        }
-
-        private void ServerStarted(object sender, ServerStartEventArgs e)
-        {
-            isServerEmbedded = e.Embedded;
-
-            if (e.Embedded)
-            {
-                LauncherLogic.Instance.NavigateTo<ServerConsolePage>();
-            }
-        }
-
-        private void ServerExited(object sender, EventArgs e)
-        {
-            Dispatcher?.Invoke(() =>
-            {
-                if (LauncherLogic.Instance.NavigationIsOn<ServerConsolePage>())
-                {
-                    LauncherLogic.Instance.NavigateTo<ServerPage>();
-                }
-            });
-        }
-
-        private void ButtonNavigation_Click(object sender, RoutedEventArgs e)
-        {
-            if (!(sender is FrameworkElement elem))
-            {
-                return;
-            }
-            LauncherLogic.Instance.NavigateTo(elem.Tag?.GetType());
-        }
-
-        private void PART_VerticalScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-
-        }
+        OnPropertyChanged();
+        OnPropertyChanged(nameof(CurrentPageBackground));
+      }
     }
+
+    public ImageSource CurrentPageBackground => (ImageSource)(FrameContent as Page)?.Background.GetValue(ImageBrush.ImageSourceProperty);
+
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      // Pirates were here
+      // Pirate trigger should happen after UI is loaded.
+      Loaded += (sender, args) =>
+      {
+        // This pirate detection subscriber is immediately invoked if pirate has been detected right now.
+        PirateDetection.PirateDetected += (o, eventArgs) =>
+        {
+          Logo.Source = new BitmapImage(new Uri("pirateNitroxLogo.png", UriKind.Relative));
+        };
+      };
+
+      logic.ServerStarted += ServerStarted;
+      logic.ServerExited += ServerExited;
+
+      logic.SetTargetedSubnauticaPath(GameInstallationFinder.Instance.FindGame())
+          .ContinueWith(task =>
+              {
+                if (logic.IsSubnauticaDirectory(task.Result))
+                {
+                  LauncherLogic.Instance.NavigateTo<LaunchGamePage>();
+                }
+                else
+                {
+                  LauncherLogic.Instance.NavigateTo<OptionPage>();
+                }
+
+                logic.CheckNitroxVersion();
+              },
+              CancellationToken.None,
+              TaskContinuationOptions.OnlyOnRanToCompletion,
+              TaskScheduler.FromCurrentSynchronizationContext());
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private bool CanClose()
+    {
+      if (logic.ServerRunning && isServerEmbedded)
+      {
+        MessageBoxResult userAnswer = MessageBox.Show("The embedded server is still running. Click yes to close.", "Close aborted", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+        return userAnswer == MessageBoxResult.Yes;
+      }
+
+      return true;
+    }
+
+    private void OnClosing(object sender, CancelEventArgs e)
+    {
+      if (!CanClose())
+      {
+        e.Cancel = true;
+        return;
+      }
+      logic.Dispose();
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+      Close();
+    }
+
+    private void Minimze_Click(object sender, RoutedEventArgs e)
+    {
+      WindowState = WindowState.Minimized;
+    }
+
+    private void ServerStarted(object sender, ServerStartEventArgs e)
+    {
+      isServerEmbedded = e.Embedded;
+
+      if (e.Embedded)
+      {
+        LauncherLogic.Instance.NavigateTo<ServerConsolePage>();
+      }
+    }
+
+    private void ServerExited(object sender, EventArgs e)
+    {
+      Dispatcher?.Invoke(() =>
+      {
+        if (LauncherLogic.Instance.NavigationIsOn<ServerConsolePage>())
+        {
+          LauncherLogic.Instance.NavigateTo<ServerPage>();
+        }
+      });
+    }
+
+    private void ButtonNavigation_Click(object sender, RoutedEventArgs e)
+    {
+      if (!(sender is FrameworkElement elem))
+      {
+        return;
+      }
+      LauncherLogic.Instance.NavigateTo(elem.Tag?.GetType());
+    }
+
+    private void PART_VerticalScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+
+    }
+  }
 }
--- NitroxLauncher/Patching/PlatformDetection.cs
+++ NitroxLauncher/Patching/PlatformDetection.cs
@@ -2,33 +2,31 @@
 
 namespace NitroxLauncher.Patching
 {
-    public class PlatformDetection
+  public static class PlatformDetection
+  {
+    public static bool IsEpic(string subnauticaPath)
     {
-        public static bool IsEpic(string subnauticaPath)
-        {
-
-            if (Directory.Exists(Path.Combine(subnauticaPath, ".egstore")))
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public static bool IsSteam(string subnauticaPath)
-        {
-            if (File.Exists(Path.Combine(subnauticaPath, "Subnautica_Data", "Plugins", "CSteamworks.dll")))
-            {
-                return true;
-            }
-            return false;
-        }
-        public static bool IsMicrosoftStore(string subnauticaPath)
-        {
-            if (File.Exists(Path.Combine(subnauticaPath, "appxmanifest.xml")))
-            {
-                return true;
-            }
-            return false;
-        }
+      if (Directory.Exists(Path.Combine(subnauticaPath, ".egstore")))
+      {
+        return true;
+      }
+      return false;
     }
+    public static bool IsSteam(string subnauticaPath)
+    {
+      if (File.Exists(Path.Combine(subnauticaPath, "Subnautica_Data", "Plugins", "CSteamworks.dll")))
+      {
+        return true;
+      }
+      return false;
+    }
+    public static bool IsMicrosoftStore(string subnauticaPath)
+    {
+      if (File.Exists(Path.Combine(subnauticaPath, "appxmanifest.xml")))
+      {
+        return true;
+      }
+      return false;
+    }
+  }
 }
